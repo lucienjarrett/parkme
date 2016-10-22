9c446f3e09edaa54b87ddf8613629b9dda0c2afd
Lucien Jarrett <ljarrett@coolcorp.com>
2016-10-22 17:28:33 -0500

    added to to index pade

--- resources/views/customertypes/index.blade.php
+++ resources/views/customertypes/index.blade.php
@@ -10,5 +10,25 @@
 @endforeach
 </ul>
 
+<table class="table">
+<thead>
+<th>#</th>  
+<th>Customer Type</th>  
+<th>Actions</th>  
+
+
+</thead>
+<tbody>
+@foreach($types as $type)
+<tr>
+
+<th> {{ $type->id }}</th>
+<td> {{ $type->name }}</td>
+<td></td>
+</tr>
+@endforeach
+</tbody>
+</table>
+
 
 @endsection()
