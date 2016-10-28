960947830e4867e077e000be98bbcd8e90054f55
Lucien Jarrett <ljarrett@coolcorp.com>
2016-10-28 14:55:42 -0500

    rebuild after update

--- config/database.php
+++ config/database.php
@@ -61,11 +61,12 @@ return [
 			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', 'parkme'),
 			'username'  => env('DB_USERNAME', 'root'),
-			'password'  => env('DB_PASSWORD', 'pass1@word'),
+			'password'  => env('DB_PASSWORD', 'root'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-			'strict'    => false
+			'strict'    => false, 
+            'socket'    => 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock',
 		],
 
   ],
